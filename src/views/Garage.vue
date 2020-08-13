3b166795493428e2064abc2b167121b90a23958e
clayton <claytonbellmor@gmail.com>
2020-08-13 15:02:46 -0400

    removed add new button
    because firebase

--- src/views/Garage.vue
+++ src/views/Garage.vue
@@ -1,11 +1,7 @@
 <template>
   <v-container>
     <h1>Garage</h1>
-    <v-row
-      v-for="(cars, index) in garage"
-      :key="index"
-      cols="12"
-    >
+    <v-row v-for="(cars, index) in garage" :key="index" cols="12">
       <v-col
         v-for="car in cars"
         :key="car.id"
@@ -16,16 +12,16 @@
         offset-sm="3"
         @click="changeProp(car, cars)"
       >
-        <v-hover
-          v-slot:default="{ hover }"
-          style="cursor: pointer"
-        >
-          <v-card
-            :elevation="hover ? 16 : 2"
-          >
+        <v-hover v-slot:default="{ hover }" style="cursor: pointer;">
+          <v-card :elevation="hover ? 16 : 2">
             <v-row>
               <v-col cols="12">
-                <h3><span style="opacity: 0.7; font-weight: lighter;">{{ car.info.year }}</span> {{ car.info.model }}</h3>
+                <h3>
+                  <span style="opacity: 0.7; font-weight: lighter;">{{
+                    car.info.year
+                  }}</span>
+                  {{ car.info.model }}
+                </h3>
               </v-col>
               <v-col>
                 <template v-if="car.garageInfo">
@@ -39,10 +35,10 @@
               </v-col>
             </v-row>
           </v-card>
-          </v-hover>
+        </v-hover>
       </v-col>
 
-      <v-col
+      <!-- <v-col
         lg="3"
         md="4"
         offset-md="0"
@@ -57,30 +53,29 @@
           <v-btn>Add New Vehicle</v-btn>
         </v-card>
 
-      </v-col>
-
+      </v-col> -->
     </v-row>
   </v-container>
 </template>
 
 <script>
-import garage from "../data/garage.json"
+import garage from "../data/garage.json";
 
 export default {
-  name: 'garage',
+  name: "garage",
   data() {
     return {
-      garage: garage
-    }
+      garage: garage,
+    };
   },
   methods: {
     changeProp(car, cars) {
       cars.map((e) => {
-        e.selectedCar=null
-      })
-      car.selectedCar=true
-      this.$router.push('/mycar')
-    }
-  }
-}
+        e.selectedCar = null;
+      });
+      car.selectedCar = true;
+      this.$router.push("/mycar");
+    },
+  },
+};
 </script>
