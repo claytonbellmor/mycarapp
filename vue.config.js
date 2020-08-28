850f4c40d4cd347cb0c500b46b3f22fde4d8081e
clayton <claytonbellmor@gmail.com>
2020-08-27 20:35:39 -0400

    Added Dockerfile
    Hash History Mode still buggy.

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
 module.exports = {
   transpileDependencies: ["vuetify"],
-  publicPath: process.env.NODE_ENV === "production" ? "/project_three/" : "/",
+  // publicPath: process.env.NODE_ENV === "production" ? "/" : "/project_three",
 };
